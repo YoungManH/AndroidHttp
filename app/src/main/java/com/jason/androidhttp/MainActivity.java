56e85cef3024156cf17e2c5d905ce948c6f6cbeb
Jason <15264316179@163.com>
2019-06-01 08:42:39 +0800

    增加HTTPUtil类

--- app/src/main/java/com/jason/androidhttp/MainActivity.java
+++ app/src/main/java/com/jason/androidhttp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.jason.androidhttp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private WebView mWebView = null;
+
+    private SendUrlTest mSendUrlTest = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mSendUrlTest = new SendUrlTest("http://www.baidu.com");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mSendUrlTest != null) {
+            mSendUrlTest.cancel(true);
+        }
+    }
+
+    private class SendUrlTest extends AsyncTask<Void, Void, String> {
+
+        public String mUrl;
+
+        public SendUrlTest(String url) {
+            mUrl = url;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return HttpUtil.sendUrl(mUrl);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mWebView.loadData(s, "text/html","charset=utf-8");
+        }
     }
 }
