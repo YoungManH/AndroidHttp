033d2e1d4e9b316108126eadf459230ecac8ddbc
Jason <15264316179@163.com>
2019-06-02 20:21:12 +0800

    增加了Image下载方法

--- app/src/main/java/com/jason/androidhttp/HttpUtil.java
+++ app/src/main/java/com/jason/androidhttp/HttpUtil.java
@@ -1,8 +1,13 @@
 package com.jason.androidhttp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -46,5 +51,50 @@ public class HttpUtil {
         }
 
         return "";
+    }
+
+    /**
+     * 返回这个URL的图片内容
+     *
+     * @param sendUrl
+     * @return
+     */
+    public static Bitmap loadImage(String sendUrl) {
+        try {
+            Log.d("loadImage:", sendUrl);
+            URL url = new URL(sendUrl);
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+
+            //设置5秒超时
+            conn.setConnectTimeout(5000);
+
+            //设置请求方式
+            conn.setRequestMethod("GET");
+            InputStream stream = conn.getInputStream();
+
+            String fileName = String.valueOf(System.currentTimeMillis());
+            FileOutputStream outputStream = null;
+            File fileDownload = null;
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                File parent = Environment.getExternalStorageDirectory();
+                fileDownload = new File(parent, fileName);
+                outputStream = new FileOutputStream(fileDownload);
+            }
+            byte[] bytes = new byte[2 * 1024];
+            int lens;
+            if (outputStream != null) {
+                while ((lens = stream.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, lens);
+                }
+                return BitmapFactory.decodeFile(fileDownload.getAbsolutePath());
+            } else {
+                return null;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- app/src/main/java/com/jason/androidhttp/MainActivity.java
+++ app/src/main/java/com/jason/androidhttp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.jason.androidhttp;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +14,20 @@ public class MainActivity extends AppCompatActivity {
     private WebView mWebView = null;
 
     private SendUrlTest mSendUrlTest = null;
+    private LoadImageUrl mLoadImageUrl = null;
+
+    private ImageView mImageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.webview);
-        mSendUrlTest = new SendUrlTest("https://www.baidu.com");
-        mSendUrlTest.execute();
+        mImageView = (ImageView) findViewById(R.id.img);
+//        mSendUrlTest = new SendUrlTest("https://www.baidu.com");
+//        mSendUrlTest.execute();
+        mLoadImageUrl = new LoadImageUrl("https://img2.mukewang.com/5b8cd82d0001c70702000200-140-140.jpg");
+        mLoadImageUrl.execute();
     }
 
     @Override
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         if (mSendUrlTest != null) {
             mSendUrlTest.cancel(true);
+        }
+        if (mLoadImageUrl != null) {
+            mLoadImageUrl.cancel(true);
         }
     }
 
@@ -46,11 +57,28 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.e("onPostExecute:","------------------------------------------");
-            Log.e("onPostExecute:",s);
-            Log.e("onPostExecute:","------------------------------------------");
+            mWebView.loadData(s, "text/html;charset=utf-8", null);
+        }
+    }
 
-            mWebView.loadData(s, "text/html;charset=utf-8",null);
+    public class LoadImageUrl extends AsyncTask<Void, Void, Bitmap> {
+        public String mUrl;
+
+        public LoadImageUrl(String url) {
+            mUrl = url;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            return HttpUtil.loadImage(mUrl);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null) {
+                mImageView.setImageBitmap(bitmap);
+            }
         }
     }
 }
