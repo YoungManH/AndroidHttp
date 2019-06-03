af87a4c199f738574f4d13a4fd466c54957b099b
Jason <15264316179@163.com>
2019-06-03 11:15:07 +0800

    权限申请添加

--- app/src/main/java/com/jason/androidhttp/MainActivity.java
+++ app/src/main/java/com/jason/androidhttp/MainActivity.java
@@ -1,15 +1,17 @@
 package com.jason.androidhttp;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private WebView mWebView = null;
 
@@ -17,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     private LoadImageUrl mLoadImageUrl = null;
 
     private ImageView mImageView = null;
+
+    private  static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private  static String[] PERMISSIONS_STORGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
         mImageView = (ImageView) findViewById(R.id.img);
 //        mSendUrlTest = new SendUrlTest("https://www.baidu.com");
 //        mSendUrlTest.execute();
+        verifyStoragePerssion(this);
         mLoadImageUrl = new LoadImageUrl("https://img2.mukewang.com/5b8cd82d0001c70702000200-140-140.jpg");
         mLoadImageUrl.execute();
+
     }
 
     @Override
@@ -79,6 +89,14 @@ public class MainActivity extends AppCompatActivity {
             if (bitmap != null) {
                 mImageView.setImageBitmap(bitmap);
             }
+        }
+    }
+
+    public  static void verifyStoragePerssion(Activity activity){
+        int writePerssion = ActivityCompat.checkSelfPermission(activity,Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int readPerssion = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(writePerssion != PackageManager.PERMISSION_GRANTED || readPerssion != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORGE, REQUEST_EXTERNAL_STORAGE);
         }
     }
 }
